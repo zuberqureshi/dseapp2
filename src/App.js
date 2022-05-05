d75124d06fd21047913638d3dd8c75559d2f08e3
zuberqureshi <zuberqureshi95@gmail.com>
2022-05-05 13:19:10 +0530

    update
    update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          zuber <code>src/App.js</code> and save to reload.
+          zuber  new <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
